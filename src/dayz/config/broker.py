5a8345510607991a4914eaef7bfb597361f578e4
Magictea <sherbakovdaniil6@gmail.com>
2025-11-26 23:50:13 +0300

    hot fix

--- src/dayz/config/broker.py
+++ src/dayz/config/broker.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings
 class BrokerConfig(BaseSettings):
     user: str = 'admin'
     password: str
-    host: str = 'amqp://admin:dayzadmin_4632@127.0.0.1:5672/'
+    host: str = 'amqp://admin:dayzadmin_4632@rabbitmq:5672/'
 
     class Config:
         env_prefix = 'BROKER_'
